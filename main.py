b26a379e8713030aff12af6e2205526cee42a080
Iago Mendes <iagobrazmendes@gmail.com>
2023-05-07 16:00:20 -0400

    Compute derivatives using FFT.

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define grid
+t_max = 10
+N = 100
+t = np.linspace(0,10,N)
+Delta = t_max / N
+
+# Define function
+h = np.exp(-(t - t_max/2)**2) # Gaussian
+
+# Computing the derivative
+H = np.fft.fft(h)
+f = np.fft.fftfreq(len(t), Delta)
+H_dot = 2 * np.pi * 1j * f * H
+h_dot = np.fft.ifft(H_dot)
+
+# Plot results
+plt.plot(t, h, label=f'$h(t)$')
+plt.plot(t, h_dot, label=f'$\dot h(t)$')
+plt.plot(t, - 2 * (t - t_max/2) * np.exp(-(t - t_max/2)**2), label=f'expected $\dot h(t)$', linestyle='dashed')
+plt.legend()
+plt.show()
