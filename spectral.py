82516f2665f2fb01695d4df1af0348bf87c3451a
Iago Mendes <iagobrazmendes@gmail.com>
2023-05-08 23:18:30 -0400

    Remove unnecessary functions.

--- spectral.py
+++ spectral.py
@@ -1,20 +1,8 @@
 import numpy as np
 
-def fft(h):
-	H = np.fft.fft(h)
-	return H
-
-def ifft(H):
-	h = np.fft.ifft(H)
-	return h
-
-def fftfreq(N, Delta):
-	f = np.fft.fftfreq(N, Delta)
-	return f
-
 def derivative(h, N, Delta):
-	H = fft(h)
-	f = fftfreq(N, Delta)
+	H = np.fft.fft(h)
+	f = np.fft.fftfreq(N, Delta)
 	H_prime = 2 * np.pi * 1j * f * H
-	h_prime = ifft(H_prime)
+	h_prime = np.fft.ifft(H_prime)
 	return h_prime.real
